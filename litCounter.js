4b0f793c22b19abf7e4ece751de2f76e4ee73daa
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-05-16 16:38:28 +0200

    add increment and decrement function to out work the min and max value of the counter
    - add create customElement of lit-counter
    
    - place the component in litCounter.html
    
    - link script tag to litCounter.js

--- litCounter.js
+++ litCounter.js
@@ -1,1 +1,39 @@
 import {LitElement, html} from 'lit';
+
+export class LitCounter extends LitElement{
+    static properties = {
+        count: {type: Number},
+    };
+
+    constructor(){
+        super();
+        this.count = 0;
+    }
+
+    render() {
+        return html `
+
+        <p><button @click = "${this._increment}">+</button></p>
+        <p> <span>${this.count}</span></p>
+        <p><button @click = "${this._decrement}">-</button></p>
+
+        `
+    }
+
+    _increment(e) {
+        let totalCount = this.count
+        const addition = parseInt(totalCount) > 0;
+        let totalCounter = Math.min(25, addition + 1)
+
+        return totalCounter;
+    }
+
+    _decrement(e) {
+        let totalCount = this.count
+        const subtration = parseInt(totalCount) < 0;
+        let totalCounter = Math.max(0, addition - 1)
+
+        return totalCounter;
+    }
+}
+customElements.define('lit-counter', LitCounter);
